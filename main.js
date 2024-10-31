94b7b367c7b5ab6987ea5c93f5fd432c611a9fe5
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-10-31 16:15:10 +0530

    jwt replacement

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 const express=require('express');
+const jwt=require('jsonwebtoken')
 const app=express();
-
+const JWT_SECRET="randomjwtsecret"
 app.use(express.json())
 
 const users=[];
 
-function generateToken(){
-    let options=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    let token="";
-    for(let i=0;i<32;i++){
-        token+=options[Math.floor(Math.random()*options.length)]
-    }
-    return token;
-}
+
 console.log(users)
 app.post('/signup', function(req, res){
     const username=req.body.username;
@@ -36,8 +30,10 @@ app.post('/signin', function(req, res){
         }
     }
     if(findUser){
-        const token=generateToken();    
-        findUser.token=token;
+        const token=jwt.sign({
+            username:username
+        }, JWT_SECRET);    
+        // findUser.token=token;
         console.log(users);
         res.json({
             token:token
@@ -50,7 +46,9 @@ app.post('/signin', function(req, res){
 })
 app.get('/me', function(req, res){
     const token=req.headers.token;
-    let findUser=users.find(user=>user.token===token);
+    const decodedInfo=jwt.verify(token, JWT_SECRET);
+    const username=decodedInfo.username
+    let findUser=users.find(user=>user.username===username);
     if(findUser){
         console.log('user found ',findUser)
         res.send({
